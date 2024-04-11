7c1cdf4248deeb400f96d74451185cb508b0cc10
wilfy7 <wilfy7am@gmail.com>
2024-04-11 10:27:36 +0200

    update

--- frontend/src/service/post.service.ts
+++ frontend/src/service/post.service.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { token } from "./user.service";
 
 const baseUrl = process.env.REACT_APP_API;
 
-//Cretae a post
+//Create a post
 export const createPost = async (contentData: any, toast: any) => {
    try {
      const res = await axios.post(`${baseUrl}/post`, contentData);
@@ -13,3 +14,33 @@ export const createPost = async (contentData: any, toast: any) => {
      toast(error);
    } 
 };
+
+//Get all Posts 
+export const getAllPosts = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}/posts/all`, {
+      headers: {
+        Authorization: token
+      }
+    });
+    return res.data.contentData
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+//Get a single post
+export const getSinglePost = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}`, {
+      headers: {
+        Authorization: token
+      }
+    });
+    return res.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
